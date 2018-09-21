79b740393e86e61b3accf6d87ab63759fef69fd0
kerwincui <164770707@qq.com>
2018-09-21 15:28:46 +0800

    add view component named message

---
+++ kewcms/Areas/Admin/ViewComponents/Message.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using kewcms.Areas.Admin.Models;
+using kewcms.Data;
+
+namespace kewcms.Areas.Admin.ViewComponents
+{
+    public class MessageViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext db;
+
+        public MessageViewComponent(ApplicationDbContext context)
+        {
+            db = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() {
+            var message = await db.Feedbacks.Where(x => string.IsNullOrWhiteSpace(x.ReplayContent)).ToListAsync();
+            return View(message);
+        }
+
+    }
+}
